61e9f68c13f6e5bc3cca2f9b15d3f24c4366db4b
Bjørn Göttler <bjoern@goettler.dk>
2025-05-09 15:08:08 +0200

    database: Lower version

---
+++ SoftwareQualityExam/Graduation/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,13 @@
+namespace Graduation.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
