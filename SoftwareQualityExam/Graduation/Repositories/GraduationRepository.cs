2878d0a92376d84804176aca51f0ec8d5d868b72
Bjørn Göttler <bjoern@goettler.dk>
2025-05-14 12:34:32 +0200

    repository: Add repository

--- SoftwareQualityExam/Graduation/Repositories/GraduationRepository.cs
+++ SoftwareQualityExam/Graduation/Repositories/GraduationRepository.cs
@@ -1,5 +1,7 @@
 using Graduation.Infrastructure;
 using Graduation.Interfaces;
+using Graduation.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Graduation.Repositories;
 
@@ -9,5 +11,40 @@ public class GraduationRepository: IGraduationRepository
     
     public GraduationRepository(DatabaseContext databaseContext)
         => _databaseContext = databaseContext;
+
+    public async Task<List<GraduationDetail>> GetAllAsync()
+        => await _databaseContext.GraduationDetails.ToListAsync();
+
+    public async Task<GraduationDetail> GetByIdAsync(int id)
+    {
+        var graduationDetail = await _databaseContext.GraduationDetails.FirstAsync(x => x.Id == id);
+        if (graduationDetail == null)
+        {
+            throw new KeyNotFoundException($"GraduationDetail with id {id} not found.");
+        }
+        return graduationDetail;
+    }
     
+    public async Task<GraduationDetail> CreateAsync(GraduationDetail graduationDetail)
+    {
+        await _databaseContext.GraduationDetails.AddAsync(graduationDetail);
+        await _databaseContext.SaveChangesAsync();
+        return graduationDetail;
+    }
+    
+    public async Task<bool> UpdateAsync(GraduationDetail graduationDetail)
+    {
+        _databaseContext.GraduationDetails.Update(graduationDetail);
+        var result = await _databaseContext.SaveChangesAsync();
+        return result > 0;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var graduationDetail = await GetByIdAsync(id);
+        _databaseContext.GraduationDetails.Remove(graduationDetail);
+        var result = await _databaseContext.SaveChangesAsync();
+        return result > 0;
+    }
+
 }
