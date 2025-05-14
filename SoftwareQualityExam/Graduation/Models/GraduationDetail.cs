eba54f8df58654251a04455ee717ffccab9b303d
Bjørn Göttler <bjoern@goettler.dk>
2025-05-14 19:35:06 +0200

    database: Add unique constraint

--- SoftwareQualityExam/Graduation/Models/GraduationDetail.cs
+++ SoftwareQualityExam/Graduation/Models/GraduationDetail.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Graduation.Models;
 
+[Index(nameof(GraduationDate), IsUnique = true)]
 public class GraduationDetail
 {
     public int Id { get; set; }
