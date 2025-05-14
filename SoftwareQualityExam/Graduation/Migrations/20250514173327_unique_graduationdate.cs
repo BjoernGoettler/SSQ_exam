85103401546572d300c0515d92f9c712a859d14f
Bjørn Göttler <bjoern@goettler.dk>
2025-05-14 19:35:41 +0200

    migrations: Add unique constraint

---
+++ SoftwareQualityExam/Graduation/Migrations/20250514173327_unique_graduationdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Graduation.Migrations
+{
+    /// <inheritdoc />
+    public partial class unique_graduationdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_GraduationDetails_GraduationDate",
+                table: "GraduationDetails",
+                column: "GraduationDate",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GraduationDetails_GraduationDate",
+                table: "GraduationDetails");
+        }
+    }
+}
